2da4719a1bd8d9520a1fd5430b29d587c2f9dcb3
Onr <restin3@gmail.com>
2019-12-06 12:00:02 +0200

    change telegram no control option after training start. fixed the telegram error with more then one program running at the same time

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
 FILE=$1
-if [ $FILE == "celeba" ]; then # taken from stargan https://github.com/yunjey/stargan
-    # CelebA images and attribute labels
+if [ $FILE == "celeba_male2female" ]; then # taken from stargan https://github.com/yunjey/stargan
+    URL=https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0
+    ZIP_FILE=./datasets/celeba.zip
+    mkdir -p ./datasets/
+    wget -N $URL -O $ZIP_FILE
+    unzip $ZIP_FILE -d ./datasets/
+    rm $ZIP_FILE
+
+elif [ $FILE == "celeba_glasses_removal" ]; then # taken from stargan https://github.com/yunjey/stargan
     URL=https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0
     ZIP_FILE=./datasets/celeba.zip
     mkdir -p ./datasets/
@@ -9,12 +17,15 @@ if [ $FILE == "celeba" ]; then # taken from stargan https://github.com/yunjey/st
     rm $ZIP_FILE
 
 elif [ $FILE == "selfie2anime" ]; then # dataset from https://github.com/taki0112/UGATIT
-    # CelebA images and attribute labels
     URL=https://www.dropbox.com/s/9lz6gwwwyyxpdnn/selfie2anime.zip?dl=0
     ZIP_FILE=./datasets/selfie2anime.zip
     mkdir -p ./datasets/
     wget -N $URL -O $ZIP_FILE
     unzip $ZIP_FILE -d ./datasets/
     rm $ZIP_FILE
+else
+    echo "Available arguments are celeba_male2female, celeba_glasses_removal, selfie2anime."
+    exit 1
+
 
 fi
