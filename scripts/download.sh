0881467eac4e375a1190733dcab4f77513114805
Onr <restin3@gmail.com>
2019-12-08 16:02:24 +0200

    adding citation

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 FILE=$1
+
+#celeba dataset
+# @inproceedings{liu2015faceattributes,
+# title = {Deep Learning Face Attributes in the Wild},
+# author = {Liu, Ziwei and Luo, Ping and Wang, Xiaogang and Tang, Xiaoou},
+# booktitle = {Proceedings of International Conference on Computer Vision (ICCV)},
+# month = {December},
+# year = {2015}
+#}
 if [ $FILE == "celeba_male2female" ]; then
     URL=https://cgm.technion.ac.il/Computer-Graphics-Multimedia/DataSet/celeba_male2female.zip?dl=0
     ZIP_FILE=./datasets/celeba_male2female.zip
@@ -16,7 +25,12 @@ elif [ $FILE == "celeba_glasses_removal" ]; then
     unzip $ZIP_FILE -d ./datasets/
     rm $ZIP_FILE
 
-elif [ $FILE == "selfie2anime" ]; then # dataset from https://github.com/taki0112/UGATIT
+#  selfie2anime dataset used in https://github.com/taki0112/UGATIT
+#  title={U-GAT-IT: Unsupervised Generative Attentional Networks with Adaptive Layer-Instance Normalization for Image-to-Image Translation},
+#  author={Kim, Junho and Kim, Minjae and Kang, Hyeonwoo and Lee, Kwanghee},
+#  journal={arXiv preprint arXiv:1907.10830},
+#  year={2019}
+  elif [ $FILE == "selfie2anime" ]; then
     URL=https://www.dropbox.com/s/9lz6gwwwyyxpdnn/selfie2anime.zip?dl=0
     ZIP_FILE=./datasets/selfie2anime.zip
     mkdir -p ./datasets/
